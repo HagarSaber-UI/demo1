f9c58f065d531b537dcbfabbc2c129fe42fc90f5
Abdullah Waell <96538981+wael1907@users.noreply.github.com>
2023-06-26 17:57:47 +0200

    changes ab

--- src/app/modules/quotes/quote/quote.module.ts
+++ src/app/modules/quotes/quote/quote.module.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { VendorRoutingModule } from './vendor-routing.module';
-import { VendorComponent } from './vendor.component';
+import { QuoteRoutingModule } from './quote-routing.module';
+import { QuoteComponent } from './quote.component';
 import {
   DropdownMenusModule,
   WidgetsModule,
 } from '../../../_metronic/partials';
 import { SharedModule } from '../../../_metronic/shared/shared.module';
-
-import { InfoComponent } from './info/info.component';
-import { LogsComponent } from './logs/logs.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { QuoteEditComponent } from './edit/edit.component';
 
 import { CardsModule } from "../../../_metronic/partials/content/cards/cards.module";
 
 @NgModule({
-    declarations: [VendorComponent,
-        InfoComponent,LogsComponent
+    declarations: [QuoteComponent,
+        LanguagesComponent,QuoteEditComponent
     ],
     imports: [
         CommonModule,
-        VendorRoutingModule,
+        QuoteRoutingModule,
         DropdownMenusModule,
         WidgetsModule,
         SharedModule,
         CardsModule
     ]
 })
-export class VendorModule {}
+export class QuoteModule {}
